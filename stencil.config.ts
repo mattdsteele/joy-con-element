a2f715002eb7d321bea89177efc2c48e2cb1a8c1
mattdsteele <orphum@gmail.com>
2019-10-14 22:07:34 -0500

    feat: experiment with angular/react bindings

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,19 @@
+import { angularOutputTarget } from '@stencil/angular-output-target';
+import { reactOutputTarget } from '@stencil/react-output-target';
 import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'joy-con-element',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'joy-con-element',
+      proxiesFile: './react-components.ts'
+    }),
+    angularOutputTarget({
+      componentCorePackage: 'joy-con-element',
+      directivesProxyFile:
+        '../joy-con-element-angular/src/directives/proxies.ts'
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader'
